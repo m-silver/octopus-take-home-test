32bd80be241e9d5539438730105f55d69c91070a
m-silver <mattsilver@windowslive.com>
2021-09-23 21:46:05 +1200

    Implement useReleaseRetention in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React from 'react'
+import useReleaseRetention from './hooks/useReleaseRetention'
 
+import Deployments from './data/Deployments.json'
+import Environments from './data/Environments.json'
+import Projects from './data/Projects.json'
+import Releases from './data/Releases.json'
 
 function App() {
+  const releaseRetentionArgs = {
+    numberOfReleases: 1,
+    deployments: Deployments,
+    environments: Environments,
+    projects: Projects,
+    releases: Releases
+  }
+  const retainedReleases = useReleaseRetention(releaseRetentionArgs)
+  
+  console.log('Retained releases: ', retainedReleases)
+
   return (
     <div className="App">
-
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
